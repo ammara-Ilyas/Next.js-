2f407cc22be9ad9409c76fb8007b278ead0dd8b7
ammara-Ilyas <ismarailyas545@gmail.com>
2024-03-14 12:31:59 +0500

    Geogle map

---
+++ location-based-next-app/src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import FacebookProvider from "next-auth/providers/facebook";
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
